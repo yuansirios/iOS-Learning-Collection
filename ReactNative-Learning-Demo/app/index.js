e73dc67b78c0e70161d74f69cadc1790ed57b9f9
yuan <695580111@qq.com>
2019-09-16 14:13:21 +0800

    删除多余文件

--- ReactNative-Learning-Demo/app/index.js
+++
@@ -1,5 +0,0 @@
-import React, {Component} from "react";
-import {AppRegistry} from "react-native";
-import InitApp from "./pages/InitApp";
-
-AppRegistry.registerComponent('ReactNativeCollection', ()=>InitApp);
